33cd63fa0cb32a3188fe156e355dc8b3ac3e215d
Danutpenciuc8 <73424508+Danutpenciuc8@users.noreply.github.com>
2021-04-26 12:12:36 +0000

    Add files via upload

---
+++ src/main/java/tasks/model/TasksOperations.java
@@ -0,0 +1,30 @@
+package tasks.model;
+
+import javafx.collections.ObservableList;
+
+import java.util.*;
+
+public class TasksOperations {
+
+    public ArrayList<Task> tasks;
+
+    public TasksOperations(ObservableList<Task> tasksList){
+        tasks=new ArrayList<>();
+        tasks.addAll(tasksList);
+    }
+
+    public Iterable<Task> incoming(Date start, Date end) throws RuntimeException{
+        ArrayList<Task> incomingTasks = new ArrayList<>();
+        if(end.before(start))
+            throw new IllegalArgumentException("End date before start date");
+        for (Task t : tasks) {
+            Date nextTime = t.nextTimeAfter(start);
+            if (nextTime != null && (nextTime.before(end) || nextTime.equals(end))) {
+                incomingTasks.add(t);
+            }
+        }
+        if(incomingTasks.isEmpty())
+            throw new RuntimeException("No results");
+        return incomingTasks;
+    }
+}
